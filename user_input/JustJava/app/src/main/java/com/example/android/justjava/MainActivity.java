cf0e760105be29af6eb94dea405f02a1b6488eeb
gauravmahal <gauravmahal3008@gmail.com>
2020-05-30 21:29:37 +0530

    added 2 options

--- user_input/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ user_input/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -7,13 +7,15 @@
  */
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -30,36 +32,106 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method is called when the order button is clicked.
      */
-    public void submitOrder(View view) {
+    /*public void submitOrder(View view) {
         //int numberOfCupOfCofee = 2;
-        display(numberOfCupOfCofee);
+        //display(numberOfCupOfCofee);
         displayMessage(numberOfCupOfCofee*5);
-    }
+    }*/
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = findViewById(R.id.quantity_text_view);
         quantityTextView.setText(String.valueOf(number));
     }
     /**
      * This method displays the given price on the screen.
      */
-    private void displayMessage(int number) {
-        String priceMessage = "Total: " + NumberFormat.getCurrencyInstance().format(number) + "\n";
-        priceMessage += "Thank you!";
-        TextView textView = findViewById(R.id.price_text_view);
+    /*private void displayMessage(int number) {
+        String priceMessage = "Total: " + NumberFormat.getCurrencyInstance().format(number) + "\n" + "Thank you!";
+        TextView textView = findViewById(R.id.order_summary_text_view);
         textView.setText(priceMessage);
-    }
+    }*/
 
     public void increment(View view) {
-        display(++numberOfCupOfCofee);
+        displayQuantity(++numberOfCupOfCofee);
         //displayPrice(numberOfCupOfCofee*5);
     }
 
     public void decrement(View view) {
-        display(--numberOfCupOfCofee);
-        //displayPrice(numberOfCupOfCofee*5);
+        if (numberOfCupOfCofee == 1) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
+        }
+        displayQuantity(--numberOfCupOfCofee);
     }
+    /*public void decrement(View view) {
+        displayQuantity(--numberOfCupOfCofee);
+        //displayPrice(numberOfCupOfCofee*5);
+    }*/
+
+    public void submitOrder(View view) {
+        // Figure out if the user wants whipped cream topping
+        CheckBox whippedCreamCheckBox = findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+
+        // Figure out if the user wants chocolate topping
+        CheckBox chocolateCheckBox = findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+
+        // Calculate the price
+        int price = calculatePrice();
+
+        // Display the order summary on the screen
+        String message = createOrderSummary(price, hasWhippedCream, hasChocolate);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java Order Summary");
+        intent.putExtra(Intent.EXTRA_TEXT,message);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+        displayMessage(message);
+    }
+
+    /**
+     * Calculates the price of the order.
+     *
+     * @return total price
+     */
+    private int calculatePrice() {
+        return numberOfCupOfCofee * 5;
+    }
+
+    /**
+     * Create summary of the order.
+     *
+     * @param price           of the order
+     * @param addWhippedCream is whether or not to add whipped cream to the coffee
+     * @param addChocolate    is whether or not to add chocolate to the coffee
+     * @return text summary
+     */
+    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate) {
+        String priceMessage = "Name: Lyla the Labyrinth";
+        priceMessage += "\nAdd whipped cream? " + addWhippedCream;
+        priceMessage += "\nAdd chocolate? " + addChocolate;
+        priceMessage += "\nQuantity: " + numberOfCupOfCofee;
+        priceMessage += "\nTotal: $" + price;
+        priceMessage += "\nThank you!";
+        return priceMessage;
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
+    }
+
 }
